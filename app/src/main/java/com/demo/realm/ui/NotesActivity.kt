58b2f358c46e6bd00b63e7ddd5c10dd4ddcbf74f
alin <alin@qburst.com>
2022-02-09 10:56:48 +0530

    Add viewModel to activity for function calls and observe liveData. Also perform some code reformat

--- app/src/main/java/com/demo/realm/ui/NotesActivity.kt
+++ app/src/main/java/com/demo/realm/ui/NotesActivity.kt
@@ -1,8 +1,7 @@
 package com.demo.realm.ui
 
 import android.os.Bundle
-import android.widget.Toast
-import androidx.annotation.NonNull
+import androidx.activity.viewModels
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -11,11 +10,13 @@ import com.demo.realm.databinding.ActivityNotesBinding
 import com.demo.realm.databinding.NoteDialogBinding
 import com.demo.realm.models.Author
 import com.demo.realm.models.Notes
+import com.demo.realm.viewModel.NotesViewModel
 
 class NotesActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityNotesBinding
     lateinit var adapter: NoteAdapter
+    val viewModel: NotesViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,11 +24,13 @@ class NotesActivity : AppCompatActivity() {
         setContentView(binding.root)
         setupAdapter()
         setupListeners()
+        observeLiveData()
+        viewModel.fetchAllNotes()
     }
 
     private fun setupListeners() {
 
-        adapter.onButtonClicks = object : NoteAdapter.OnButtonClicks{
+        adapter.onButtonClicks = object : NoteAdapter.OnButtonClicks {
 
             override fun onUpdateClicked(notes: Notes) {
                 showInsertDialog(true, notes)
@@ -57,39 +60,63 @@ class NotesActivity : AppCompatActivity() {
     private fun showInsertDialog(isUpdate: Boolean = false, notes: Notes? = null) {
         val dialogViewBinding = NoteDialogBinding.inflate(layoutInflater)
         val alertDialog = AlertDialog.Builder(this)
-        alertDialog.apply{
+        alertDialog.apply {
             setTitle(if (isUpdate) R.string.update_notes else R.string.add_notes)
             setView(dialogViewBinding.root)
-            setPositiveButton(R.string.cancel){ dialog, _ ->
+            if (isUpdate) notes?.let { dialogViewBinding.populateForm(it) }
+            setPositiveButton(R.string.cancel) { dialog, _ ->
                 dialog.cancel()
             }
-            setNegativeButton(if (isUpdate) R.string.update else R.string.add){ dialog, _ ->
-                setNegativeButtonAction(isUpdate, dialogViewBinding)
+            setNegativeButton(if (isUpdate) R.string.update else R.string.add) { dialog, _ ->
+                val id = notes?.id ?: (viewModel.lastId + 1)
+                setNegativeButtonAction(isUpdate, dialogViewBinding, id)
                 dialog.cancel()
             }
         }.create().show()
     }
 
+    private fun NoteDialogBinding.populateForm(notes: Notes) {
+        nameInputEditText.setText(notes.author.name)
+        placeInputEditText.setText(notes.author.place)
+        noteInputEditText.setText(notes.message)
+    }
+
+    private fun NoteDialogBinding.getFormData(id: Int): Notes {
+        val author = Author(
+            nameInputEditText.text.toString(),
+            placeInputEditText.text.toString()
+        )
+        return Notes(
+            id,
+            noteInputEditText.text.toString(),
+            author
+        )
+    }
+
     private fun setNegativeButtonAction(
         isUpdate: Boolean,
-        dialogViewBinding: NoteDialogBinding
+        dialogViewBinding: NoteDialogBinding,
+        id: Int
     ) {
-        val notes: Notes
-        if (!isUpdate) {
-            with(dialogViewBinding) {
-                val author = Author(
-                    nameInputEditText.text.toString(),
-                    placeInputEditText.text.toString()
-                )
-                notes = Notes(
-                    1,
-                    noteInputEditText.text.toString(),
-                    author
-                )
-            }
-            Toast.makeText(this@NotesActivity, "$notes", Toast.LENGTH_SHORT).show()
+        val notes = dialogViewBinding.getFormData(id)
+        if (isUpdate) {
+            viewModel.updateNotes(notes)
+        } else {
+            viewModel.insertNote(notes)
         }
     }
 
+    private fun observeLiveData() {
+
+        viewModel.operationLiveData.observe(this) {
+            if (it) viewModel.fetchAllNotes()
+        }
+
+        viewModel.notesList.observe(this) {
+            viewModel.lastId = if (it.isNotEmpty()) it.last().id else 0
+            adapter.submitList(it)
+        }
+
+    }
 
 }
